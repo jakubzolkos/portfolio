2d3e2ae9803dcab7f2f3fd3467bac1301447e8c8
jakubzolkos <zolkos@bu.edu>
2023-02-08 17:37:10 -0500

    Changed local paths, minor style changes

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -127,7 +127,7 @@ const Work = () => {
 								</h3>
 								<div className="project_desc bg-[#16171d] bg-opacity-80 bg-none">
 									<p className="bg-none">
-										<p className='font-bold'>PROJECT IN PROGRESS. STAY TUNED!</p> <br></br>Partying on a Friday night? DrunkAPI is a web app where artifficial intelligence checks whether you are sober or had
+										<p className='font-bold bg-none'>PROJECT IN PROGRESS. STAY TUNED!</p> <br></br>Partying on a Friday night? DrunkAPI is a web app where artifficial intelligence checks whether you are sober or had
 										a little to many drinks, solely based on the voice in the uploaded recording. 
 									</p>
 									<div className="odd_links flex justify-end bg-none">
