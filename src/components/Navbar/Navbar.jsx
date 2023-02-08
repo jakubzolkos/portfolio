d309337d11365ab20faa98bf68ae9f4dec241ad2
jakubzolkos <zolkos@bu.edu>
2023-02-08 16:50:44 -0500

    Minor changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,6 @@ import useScroll from "../../hooks/useScroll";
 import "./Navbar.scss";
 import resume from "../../assets/jakub_zolkos_resume.pdf"
 
-const Logo = () => {
-	return (
-		<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="100" height="100"><path fill="#F92672" d="m46.63 48.077-11.422 6.527v3.853l3.362 1.92 11.427-6.529 11.43 6.529 3.368-1.924v-3.848l-11.426-6.524V35.02l-3.37-1.925-3.369 1.925v13.056z"/><path fill="#3A3C4E" d="M50 10 15 30v40l35 20 34.99-19.994.01-.016V30L50 10zm0 7.699 24.891 14.22-6.732 3.849L50 25.396 31.846 35.771l-6.739-3.852L50 17.699zm-3.37 62.678L21.734 66.152V37.691l6.734 3.849v20.761L46.63 72.68v7.697zm-11.422-21.92V41.546l14.791-8.451 14.796 8.451v16.907L50 66.904l-14.792-8.447zm18.161 21.92V72.68l18.16-10.375V41.54l6.734-3.849v28.461h.002L53.369 80.377z"/></svg>
-	);
-};
-
-
 const Navbar = () => {
 	const { scrollY, scrollDirection } = useScroll();
 
@@ -31,7 +24,7 @@ const Navbar = () => {
 					data-aos="zoom-in"
 					data-aos-duration="700"
 				>
-					<a href="#header"><img className = "bg-none"alt=""/><Logo/></a>
+					<a href="#header"><img className = "bg-none" src={"../../logo.svg"} alt=""/></a>
 				</div>
 
 				<div className="right bg-none">
